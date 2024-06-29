db37ed1fe9cb0632af6837a009b53da07afff541
Hoang Sang <st319913@gmail.com>
2024-06-29 15:39:59 +0700

    fix form

--- pages/form-submit/form.js
+++ pages/form-submit/form.js
@@ -3,7 +3,15 @@ Page({
     weekOptions: ['Tuần 1', 'Tuần 2', 'Tuần 3', 'Tuần 4'],
     selectedWeek: 'Tuần 1',
     importantOptions: ['A', 'B', 'C'],
-    selectedImportant: 'A'
+    selectedImportant: 'A',
+    categoryOptions: ['Việc Chính', 'Dự án', 'Việc phát sinh', 'Việc cần đôn đốc', 'Đọc & học'],
+    selectedCategory: 'Việc Chính',
+    urgentOptions: ['1', '2', '3'],
+    selectedurgent: '1',
+    selectedDate1: '', // Thêm selectedDate để lưu ngày và giờ được chọn
+    selectedTime1: '', // Thêm selectedTime để lưu ngày và giờ được chọn
+    selectedDate2: '', // Thêm selectedDate để lưu ngày và giờ được chọn
+    selectedTime2: '', // Thêm selectedTime để lưu ngày và giờ được chọn
   },
 
   onWeekChange: function (e) {
@@ -16,5 +24,58 @@ Page({
     this.setData({
       selectedImportant: this.data.importantOptions[e.detail.value]
     });
+  },
+
+  onCategoryChange: function (e) {
+    this.setData({
+      selectedCategory: this.data.categoryOptions[e.detail.value]
+    });
+  },
+
+  onUrgentChange: function (e) {
+    this.setData({
+      selectedurgent: this.data.urgentOptions[e.detail.value]
+    });
+  },
+
+  onDateChange1: function (e) {
+    this.setData({
+      selectedDate1: e.detail.value
+    });
+  },
+  // onDateChange1: function (e) {
+  //   const selectedDate = e.detail.value; // Get selected date in YYYY-MM-DD format
+
+  //   // Get selected time from another input field or event (e.g., time picker)
+  //   const timePicker = document.getElementById('timePicker'); // Replace with your time picker's ID
+
+  //   timePicker.addEventListener('timeChange', (event) => {
+  //     const selectedTime = event.detail.selectedTime; // Replace with the property name containing the selected time
+  //     // ... Rest of the code to combine date and time
+  //   });
+  //   // Combine date and time into a single timestamp or formatted string
+  //   const dateTime = new Date(`${selectedDate} ${timePicker}`); // Example: Combine date and time
+
+  //   this.setData({
+  //     selectedDate1: dateTime.toISOString() // Store combined date and time in ISO format
+  //   });
+  // },
+
+  // onTimeChange1: function (e) {
+  //   this.setData({
+  //     selectedTime1: e.detail.value
+  //   });
+  // },
+
+  onDateChange2: function (e) {
+    this.setData({
+      selectedDate2: selectedDate2.datetimepicker()
+    });
+  },
+
+  onTimeChange2: function (e) {
+    this.setData({
+      selectedTime2: e.detail.value
+    });
   }
-})
+});
