397965d15338dd6ea324d8083e89fcce62023fa9
thanhcd <chauducthanh02@gmail.com>
2024-07-07 16:28:02 +0700

    Fix css 2 form, add combobox quyền

--- pages/form-submit2/event.js
+++ pages/form-submit2/event.js
@@ -11,6 +11,8 @@ Page({
     inviteData: [],
     frequencyOptions: ['Hàng ngày', 'Hàng tuần', 'Hàng tháng '],
     selectedFrequency: 'Hàng ngày',
+    permissionOptions : ['Chỉ xem', 'Được mời', 'Được sửa', 'Không'],
+    selectedPermission : 'Không',
     calendarID: '',
     lich: [],
     chonlich: '',
@@ -44,6 +46,12 @@ Page({
   onFrequencyChange: function (e) {
     this.setData({
       selectedFrequency: this.data.frequencyOptions[e.detail.value]
+    });
+  },
+
+  onPermissionChange: function (e) {
+    this.setData({
+      selectedPermission: this.data.permissionOptions[e.detail.value]
     });
   },
 
